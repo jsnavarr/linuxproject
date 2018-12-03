a7dad0e66c69eebc8eeb90cede97891edc64b64a
jsnavarr <salvador.navarro@gmail.com>
2018-12-03 21:56:28 +0000

    deleted images when catalog items or categories are deleted

--- catalog.py
+++ catalog.py
@@ -285,6 +285,21 @@ def deleteCategory(category_name):
         # If user is the owner then he is allowed to delete this item
         if categoryToDelete.user_id == login_session['user_id']:
             if request.method == 'POST':
+                # Get the catalog items on this category so the images
+                # can be deleted too
+                items = session.query(CatalogItem).filter_by(
+                    category_id=categoryToDelete.id).all()
+                if items:
+                    # Get the images on each catalog item and delete them
+                    # from the database and the file system
+                    for i in items:
+                        imgs = session.query(CatalogItemImg).filter_by(
+                                catalogItem_id=i.id).all()
+                        for img in imgs:
+                            fileToRemove = os.path.join(app.config['UPLOAD_FOLDER'], img.uuid_prefix+img.name)
+                            os.remove(fileToRemove)
+                            session.query(CatalogItemImg).filter_by(
+                                catalogItem_id=i.id).delete()
                 session.delete(categoryToDelete)
                 session.query(CatalogItem).filter_by(
                     category_id=categoryToDelete.id).delete()
@@ -483,6 +498,10 @@ def deleteCatalogItem(category_name, item_title):
                     imgs = session.query(CatalogItemImg).filter_by(
                         catalogItem_id=itemToDelete.id).all()
                     if request.method == 'POST':
+                        # Delete the actual image files
+                        for img in imgs:
+                            fileToRemove = os.path.join(app.config['UPLOAD_FOLDER'], img.uuid_prefix+img.name)
+                            os.remove(fileToRemove)
                         session.delete(itemToDelete)
                         session.query(CatalogItemImg).filter_by(
                             catalogItem_id=itemToDelete.id).delete()
@@ -740,6 +759,7 @@ def deleteCatalogItemImage(category_name, item_title, img_name):
                 if item.user_id == login_session['user_id']:
                     if request.method == 'POST':
                         fileToRemove = os.path.join(app.config['UPLOAD_FOLDER'], img.uuid_prefix+img.name)
+                        print fileToRemove
                         os.remove(fileToRemove)
                         session.delete(img)
                         session.commit()
